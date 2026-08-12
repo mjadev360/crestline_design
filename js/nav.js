7d16e88dd8176a89dcd51d9b1140d6ddf9113b1e
Matthew <mattlokk@gmail.com>
2026-08-12 16:27:59 -0700

    initial commit

---
+++ js/nav.js
@@ -0,0 +1,34 @@
+const toggle = document.querySelector(".nav-toggle");
+const nav = document.querySelector(".site-nav");
+
+if (toggle && nav) {
+  toggle.addEventListener("click", () => {
+    const open = nav.classList.toggle("is-open");
+    toggle.setAttribute("aria-expanded", String(open));
+  });
+}
+
+const form = document.querySelector(".contact-form");
+
+if (form) {
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const success = form.querySelector(".form-success");
+
+    fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: { "Content-Type": "application/json", Accept: "application/json" },
+      body: JSON.stringify(Object.fromEntries(new FormData(form))),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          form.reset();
+          if (success) success.hidden = false;
+        } else {
+          alert("Something went wrong. Please try again.");
+        }
+      })
+      .catch(() => alert("Something went wrong. Please try again."));
+  });
+}
